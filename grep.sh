fad870ae4d6275958e468766fa0032637c3d000b
laurentsimon <64505099+laurentsimon@users.noreply.github.com>
2022-07-01 12:44:05 -0700

    Update grep.sh

--- grep.sh
+++ grep.sh
@@ -6,7 +6,7 @@ set -e
 # Note: splitting command in two to avoid using pipe because
 # we set -o pipefail option and grep returns 1 when there is no match
 
-results=$(grep -r --include='*.yml' --include='*.yaml' -e 'actions/checkout@\|actions/checkout-go@' .github/workflows/* | grep -v 'checkout-go\|generate-builder')
+results=$(grep -r --include='*.yml' --include='*.yaml' -e 'dactions/checkout@\|dactions/checkout-go@' .github/workflows/* | grep -v 'checkout-go\|generate-builder')
 if [[ "$results" != "" ]]; then
     echo "Some Actions are using 'actions/checkout'"
     echo "$results"
