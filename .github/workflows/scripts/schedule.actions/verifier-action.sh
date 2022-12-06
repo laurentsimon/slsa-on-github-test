3fb793e1dcf4bb9a744d03fbf619c7a35bd46b7a
laurentsimon <laurentsimon@google.com>
2022-12-06 19:40:40 +0000

    update
    Signed-off-by: laurentsimon <laurentsimon@google.com>

--- .github/workflows/scripts/schedule.actions/verifier-action.sh
+++ .github/workflows/scripts/schedule.actions/verifier-action.sh
@@ -10,7 +10,8 @@ minimum_version="v1.3.1"
 list="\"$minimum_version\""
 # Check the releases.
 echo "Listing releases"
-release_list=$(gh release list)
+# Note: can remove -R option.
+release_list=$(gh -R slsa-framework/slsa-verifier release list)
 while read -r line; do
     tag=$(echo "$line" | cut -f1)
     if version_ge "$tag" "$minimum_version"; then
