39cbfd2d4cc93b407d9a40eec12e3a78da4723e2
laurentsimon <laurentsimon@google.com>
2022-12-06 19:10:44 +0000

    update
    Signed-off-by: laurentsimon <laurentsimon@google.com>

---
+++ .github/workflows/scripts/schedule.actions/verifier-action.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# shellcheck source=/dev/null
+source "./example-package/.github/workflows/scripts/e2e-utils.sh"
+
+#GH=~/slsa/slsa-github-generator/gh/gh_2.9.0_linux_amd64/bin/gh
+
+minimum_version="v1.3.1"
+list="\"$minimum_version\""
+# Check the releases.
+echo "Listing releases"
+release_list=$($GH release list)
+while read -r line; do
+    tag=$(echo "$line" | cut -f1)
+    if version_ge "$tag" "$minimum_version"; then
+        echo " INFO: found version to test: $tag"
+        list="$list, \"$tag\""
+    fi
+done <<<"$release_list"
+
+versions="[$list]"
+echo "version=$versions" >> "$GITHUB_OUTPUT"
+#echo "::set-output name=version::$versions"
