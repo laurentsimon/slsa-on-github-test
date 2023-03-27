d8f35d66696383e1de4094aaaedd66bd3992d329
laurentsimon <64505099+laurentsimon@users.noreply.github.com>
2023-03-27 11:17:50 -0700

    Create secure-upload-folder.sh

---
+++ .github/workflows/scripts/schedule.actions/secure-upload-folder.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -euo pipefail
+
+compare_trees() {
+    local upload_tree="$1"
+    local download_tree="$2"
+    if [[ "${upload_tree}" != "${download_tree}" ]]; then
+        echo "Folder trees differ: ${upload_tree} != ${download_tree}"
+        echo "Upload tree: ${upload_tree}"
+        echo "Download tree: ${download_tree}"
+        exit 1
+    fi
+}
+
+original_wd="$PWD"
+
+# Folder at the root of GITHUB_WORKSPACE.
+tree "${DOWNLOAD_FOLDER_NAME}"
+cd "${UPLOAD_FOLDER_NAME}"
+upload_tree=$(tree .)
+cd "../${DOWNLOAD_FOLDER_NAME}/${UPLOAD_FOLDER_NAME}"
+download_tree=$(tree .)
+
+compare_trees "${upload_tree}" "${download_tree}"
+
+# Folder not at the root of GITHUB_WORKSPACE.
+cd "${original_wd}"
+tree "${DOWNLOAD_FOLDER_NO_ROOT_NAME}"
+cd "${UPLOAD_FOLDER_NO_ROOT_NAME}"
+upload_tree=$(tree .)
+cd "../${DOWNLOAD_FOLDER_NO_ROOT_NAME}/${UPLOAD_FOLDER_NO_ROOT_NAME}"
+download_tree=$(tree .)
+
+compare_trees "${upload_tree}" "${download_tree}"
